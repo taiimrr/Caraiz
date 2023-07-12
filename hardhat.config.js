4c5214c2ee37584d728566e0ba49a9fad19b3a5a
taiimrr <taimoornasir590@gmail.com>
2023-07-12 14:56:51 +0500

    Pinata and Alchemy settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
