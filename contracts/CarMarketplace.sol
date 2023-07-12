e494e7eb2f6f96f0d3d1d7f2263b8276f953e95b
taiimrr <taimoornasir590@gmail.com>
2023-07-12 17:09:26 +0500

    Added chainlink for     using PriceConverter for uint256;

--- contracts/CarMarketplace.sol
+++ contracts/CarMarketplace.sol
@@ -6,8 +6,11 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract CarMarketplace is ERC721URIStorage {
+    using PriceConverter for uint256;
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _carsRegistered;
